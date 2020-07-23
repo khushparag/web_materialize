736b96d379a85cbe98c4b94e33d9ddd2fa869840
khushparag <parag.kachhawa@gmail.com>
2020-07-23 23:08:32 +0530

    materilizecss added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'wesitematerilize';
+export class AppComponent implements OnInit {
+  options = { fullWidth: false };
+
+  hrefs = ['one', 'two', 'three', 'four', 'five'];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    // ERROR
+    // Error: Cannot read property 'clientWidth' of undefined
+    // let elems = document.querySelectorAll('.carousel');
+    // let instances = M.Carousel.init(elems, this.options);
+  }
 }
