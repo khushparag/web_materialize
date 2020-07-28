17f0b6138a31518ee656759fc8141b38f559e1a4
khushparag <parag.kachhawa@gmail.com>
2020-07-28 17:48:23 +0530

    navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,28 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import * as M from 'materialize-css/js/sidenav';
 
+declare const M: any;
+// declare var fillSidenav:any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  options = { fullWidth: false };
-
-  hrefs = ['one', 'two', 'three', 'four', 'five'];
-
-  constructor(private http: HttpClient) {}
+export class AppComponent implements OnInit, AfterViewInit {
+  options = { edge: screenLeft };
+  constructor() {}
 
   ngOnInit() {
     // ERROR
     // Error: Cannot read property 'clientWidth' of undefined
     // let elems = document.querySelectorAll('.carousel');
     // let instances = M.Carousel.init(elems, this.options);
+  }
+  ngAfterViewInit() {
+    // no errors
+    setTimeout(function () {
+      var elem = document.querySelector('.sidenav');
+      var instance = M.Sidenav.init(elem, this.options);
+    }, 0);
   }
 }
---
+++ src/assets/js/sidenav.js
@@ -0,0 +1,6 @@
+function fillSidenav() {
+  document.addEventListener("DOMContentLoaded", function () {
+    var elems = document.querySelectorAll(".sidenav");
+    var instances = M.Sidenav.init(elems, options);
+  });
+}
